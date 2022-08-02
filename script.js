171c8399eb5654840db2649f5fd50d0e26cc3691
Vlad <vladikhoncharuk@gmail.com>
2022-08-02 16:46:14 +0300

    add delete-list

--- script.js
+++ script.js
@@ -12,16 +12,35 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     document.querySelector('form').onsubmit = () => {
+
+        const labelElement = document.createElement('label'),
+        inputElement = document.createElement('input');
+        spanElement = document.createElement('span');
+
+        inputElement.type = "checkbox";
+        spanElement.classList.add("checkbox");
+
+        labelElement.appendChild(inputElement);
+        labelElement.appendChild(spanElement);
+
+
+        const deleteButton = document.querySelector('#delete');
         const task = document.querySelector('#task').value;
-        console.log(task);
 
         const li = document.createElement('li');
         li.innerHTML = task;
+        li.classList.add("task-li");
+        li.appendChild(labelElement);
 
         document.querySelector('#tasks').append(li);
 
         document.querySelector('#task').value = '';
         document.querySelector('#submit').disabled = true;
+
+        deleteButton.addEventListener('click', () => {
+            document.querySelector('#tasks').removeChild(li);
+            labelElement.style.display = "none";
+        })
 
         return false;
     }
